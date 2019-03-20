24326e8e9ba171d3df7c6a50462c7ea167a3ebb6
Johannes Kreutz <johannes@JKMs-iHack.local>
2019-03-20 14:25:59 +0100

    Add servermanager pages

---
+++ ui/servermanager.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<?php
+    $page = 'Servermanagement';
+    include "../api/dbconnect.php";
+    session_start();
+    if ($_SESSION['user'] == null || $_SESSION['user'] == '' || ($_SESSION['timeout'] + 1200) < time()) {
+        header("Location: nologin.php");
+    } elseif ($_SESSION['type'] != '1' && $_SESSION['type'] != '3') {
+        header("Location: restricted.php");
+    } else {
+        $_SESSION['timeout'] = time();
+        include "menue.php";
+        include "../api/accessConfig.php";
+    }
+?>
+<html lang="de">
+<head>
+    <title>Servermanagement - PhilleConnect Admin</title>
+    <?php include "includes.php"; ?>
+</head>
+<body>
+    <?php include "assets/preloader.php"; ?>
+    <div role="navigation" id="foo" class="nav-collapse">
+        <div class="top">
+            <img src="ressources/img/logo.png">
+            <li><b>PHILLE</b>CONNECT</li>
+        </div>
+        <ul>
+            <?php
+                echo $menu;
+            ?>
+        </ul>
+        <?php include "assets/timeout.php"; ?>
+    </div>
+    <div role="main" class="main">
+        <a href="#nav" class="nav-toggle">Menu</a>
+        <noscript>
+            <p>Dein Browser unterstützt kein JavaScript oder JavaScript ist ausgeschaltet. Du musst JavaScript aktivieren, um diese Seite zu verwenden!</p>
+        </noscript>
+        <p style="font-family: Arial, sans-serif; font-size: 45px; text-transform: uppercase;"><b>SERVER</b>MANAGEMENT</p>
+        
+    </div>
+    <script>
+        var navigation = responsiveNav("foo", {customToggle: ".nav-toggle"});
+
+    </script>
+</body>
+</html>
